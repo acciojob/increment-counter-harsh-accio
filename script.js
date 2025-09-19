86860766f3ea48cf09ac5e9d243f7da9130519b5
harsh-accio <rajputharsh9897@gmail.com>
2025-09-19 12:31:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-//your JS code here. If required.
-let value=0
-function increase() {
-	value++;
-	document.getElementById('counter').innerHTML=value;
-	alert(value);
+let value = 0;
+
+// Function to increment counter
+function incrementCounter() {
+  // Display alert with the current (un-incremented) value
+  alert(value);
+
+  // Increment the counter value
+  value++;
+
+  // Update the counter paragraph
+  document.getElementById('counter').innerText = value;
 }
+
+// Attach the function to the button click
+document.getElementById('incrementBtn').addEventListener('click', incrementCounter);
